394709d4e10e0372b237084eb6ee9f8bb7dd34e5
Payas17 <payas17@hotmail.com>
2015-11-07 15:07:11 -0300

    probando commit con intellij

--- test/fiuba/algo3/tests/PruebasEnvidoTest.java
+++ test/fiuba/algo3/tests/PruebasEnvidoTest.java
@@ -1,7 +1,15 @@
 package fiuba.algo3.tests;
 
+import org.junit.Test;
+
 /**
  * Created by Lucio on 07/11/2015.
  */
+
 public class PruebasEnvidoTest {
+
+    @Test
+    public void testTengo1DeEspada7DeEspadaY5DeBastoYCuenta28DeEnvido(){
+
+    }
 }
