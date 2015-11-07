7ec21f739f0cbf94421f17e8cc49ee3a476138ad
Payas17 <payas17@hotmail.com>
2015-11-07 20:33:09 -0300

    anda el envido - probando jenkins

--- test/fiuba/algo3/tests/PruebasEnvidoTest.java
+++ test/fiuba/algo3/tests/PruebasEnvidoTest.java
@@ -1,9 +1,11 @@
 package fiuba.algo3.tests;
 
 
+
 import modelo.*;
 import org.junit.Assert;
 import org.junit.Test;
+
 
 /**
  * Created by Lucio on 07/11/2015.
@@ -12,7 +14,7 @@ import org.junit.Test;
 public class PruebasEnvidoTest {
 
     @Test
-    public void testTengo1DeEspada7DeEspadaY5DeBastoYCuenta28DeEnvido(){
+    public void testTengo1DeOro7DeOroY7DeEspadaYCuenta28DeEnvido(){
         Jugador jugador = new Jugador();
         Carta oro7 = new Oro(7);
         Carta oro1 = new Oro(1);
@@ -23,6 +25,22 @@ public class PruebasEnvidoTest {
         jugador.agregarCarta(espada1);
 
         Assert.assertEquals(jugador.cantarEnvido(), 28);
+
+    }
+
+
+    @Test
+    public void testTengo1DeEspada10DeEspadaY5DeBastoYCuenta28DeEnvido(){
+        Jugador jugador = new Jugador();
+        Carta espada1 = new Espada(1);
+        Carta espada10 = new Espada(0);
+        Carta basto5 = new Basto(5);
+
+        jugador.agregarCarta(espada10);
+        jugador.agregarCarta(basto5);
+        jugador.agregarCarta(espada1);
+
+        Assert.assertEquals(jugador.cantarEnvido(), 21);
 
     }
 }
