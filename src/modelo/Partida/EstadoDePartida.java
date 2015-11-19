3a186749801fa39081311f59d8174eeb717924c0
Augusto <turi.77@hotmail.com>
2015-11-18 22:10:55 -0300

    Arrancamos partida

---
+++ src/modelo/Partida/EstadoDePartida.java
@@ -0,0 +1,7 @@
+package modelo.Partida;
+
+/**
+ * Created by Augusto on 18/11/2015.
+ */
+public interface EstadoDePartida {
+}
---
+++ src/modelo/Partida/Partida.java
@@ -0,0 +1,18 @@
+package modelo.Partida;
+
+import modelo.Equipo;
+import modelo.Jugada;
+import modelo.Mazo;
+
+import java.util.List;
+
+/**
+ * Created by Augusto on 18/11/2015.
+ */
+public class Partida {
+    Equipo equipo1;
+    Equipo equipo2;
+    Mazo mazoDeCartas;
+    List<Jugada> jugadas;
+    Partida.EstadoDePartida estadoDePartida;
+}
